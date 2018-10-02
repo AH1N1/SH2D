83bce55e09a1697a7246a77062ace0ccdd36c371
AH1N1 <wolf11-1995@o2.pl>
2018-10-03 00:04:00 +0200

    bron- dodanie metod + zalozenia

---
+++ src/weapons/weapon/SomeWeapon.js
@@ -0,0 +1,55 @@
+/**
+* ZALOZENIA
+ * - bron ma jakis image
+ * - kazda bron ma zdefiniowane jakimi pociskami strzela, ile ich ma, i ile zajmuje przeladowanie
+ * - bron ktora lezy gdzies sie na mapie dostaje jakas pozycje x i y, oraz jest wyswietlana
+ * - bron ktora jest przez kogos uzywana dostaje flage inUse
+ * - tablica broni, przechowuje bronie z flaga inUse oraz te bez niej. gdy boty lub gracz podnosza bron flaga jest ustawiana, gdy upuszczaja zdejmowana, gdy bron sie zniszczy jest usuwana z gry
+ * - gdy bot upusci bron, z tablicy brana jest pierwsza bron z flaga in use, przekazywana jest jej pozostala amunicja, a flaga jest zdejmowana
+ * - gry ktos podnosci bron z tablicy brana jest pierwsza bron danego typu bez flagi inUse, bot/gracz pobiera z niej info o typie i ilosci amunicji, flaga inUse jest ustawiana
+ * - bron mozna podniesc tylko jesli jest w X odleglosci od gracza
+ * - bron jest obiektem ktory ma jakis image, zeby byl klikalny, a za zadanie ma przekazywac informacje temu kto ja podniesie o typie i ilosci amunicji
+ * - w momencie podniesienia bron przestaje byc wyswietlana, a gracz dostaje do dyspozycji X amunicji danego typu, ktora bedzie sobie spawnil.
+ * - po upuszczeniu przez bota pron dostaje jego pozycje i znow zaczyna byc widoczna, oraz przekazywane jest z powrotem info o ilosci amunicji
+ * - po rzuceniu przez gracza bron dostaje jego pozycje, oraz jakas predkosc, oraz zaczyna byc wyswietlana + dostaje cialo, ktore bedzie kolidowac z botami oraz otoczeniem
+*
+*
+* */
+
+
+class SomeWeapon extends Phaser.GameObjects.Sprite {
+
+    constructor(scene) {
+        super(scene, 200, 200, 'someWeapon');
+    }
+
+    preUpdate(time, delta){
+        this.x +=3;
+        this.y +=3;
+    }
+
+    //Ustawia podnoszacemu info o amunicji- ilosc i typ-
+    pickUp(osobaPodnoszaca){}
+
+
+    //nadaje widzocznosc/ ustawia info o pozostalej aminicji -ilosc i params to albo pojedyncze wartosci albo osoba podnoszacaK
+    dropWeapon(ammoLeft, x, y){}
+
+    //ustawia wartosc flagi inUse true/false
+    setInUse(value){
+        this.inUse = value;
+    }
+
+    getInUse(){
+        return this.inUse;
+    }
+    //TODO: wiecej parametrow- ustal
+    //Ustawia widocznosc i dodaje cialo. Nadaje broni jakas predkosc w okreslonym kierunku(dodacv w parametrach)
+    throwWeapon(x, y, velocityX, velocityY){}
+
+    //Usuwa sie z grupy / params do ustalenia
+    destroyWeapon(scene, group){}
+
+
+
+}
